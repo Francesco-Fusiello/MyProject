9e4f7bc011c3163e52bb02caa7356682989d9b3f
LuanaCanozzi <luana.canozzi@tiscali.it>
2024-04-04 21:40:50 +0200

    create relazioni nei model e fillable

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Announcement extends Model
 {
     use HasFactory;
+    protected $fillable=['title', 'body', 'price', 'id_category', 'id_user'];
+
+
+    public function category(){
+        return $this->belongsTo(category::class);
+    }
 }
--- app/Models/category.php
+++ app/Models/category.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Announcement;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class category extends Model
 {
     use HasFactory;
+    protected $fillable=['title', 'body'];
+
+    public function announcements(){
+        return $this->hasMany(Announcement::class);
+    }
 }
