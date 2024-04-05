440ae73c82894babb853eed21199ce342920b97b
Franesco Fusiello <ffusiello22@gmail.com>
2024-04-05 18:06:01 +0200

    Aggiunte Validate

--- app/Livewire/CreateAnnouncement.php
+++ app/Livewire/CreateAnnouncement.php
@@ -4,14 +4,23 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Announcement;
+use Livewire\Attributes\Validate;
 
 class CreateAnnouncement extends Component
 {
+    #[Validate('required', message:'Il titolo è richiesto')]
+    #[Validate('max:50', message:'Il titolo è troppo lungo')] 
     public $title;
+    #[Validate('required',message:'La descrizione è richiesta')]
+    #[Validate('max:300', message:'La descrizione è troppo lunga')]  
     public $body;
+    #[Validate('required', message:'Il prezzo è richiesto')] 
     public $price;
 
     public function store(){
+
+        $this->validate();
+
         Announcement::create([
             'title'=>$this->title,
             'body'=>$this->body,
--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -5,16 +5,25 @@
         <div class="mb-3">
             <label  class="form-label">Titolo</label>
             <input wire:model.change='title' type="text" class="form-control">
+            @error('title')
+            <span class="text-danger">{{$message}}</span>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label  class="form-label">Descrizione</label>
             <input wire:model.change='body' type="text" class="form-control">
+            @error('body')
+            <span class="text-danger">{{$message}}</span>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label  class="form-label">Prezzo</label>
             <input wire:model.change='price' type="number" class="form-control">
+            @error('price')
+            <span class="text-danger">{{$message}}</span>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Crea</button>
