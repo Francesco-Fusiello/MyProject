de14227a7e3acb8c330abe9886b806487aa0d07e
Franesco Fusiello <ffusiello22@gmail.com>
2024-04-08 18:21:08 +0200

    Modificato area revisore

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,5 +1,4 @@
 <x-layout>
-    <h1>Area Revisore</h1>
     <div class='container-fluid p-5 bg-gradient bg-success p-5 shadow mb-4'>
         <div class="row">
             <div class="col-12 text-light p-5">
@@ -20,15 +19,15 @@
                             <div class="carousel-item active">
                                 <img src="https://picsum.photos/id/27/1200/400" class="img-fluid p-3 rounded"
                                     alt="...">
-                            </div>
-                            <div class="carousel-item">
+                                </div>
+                                <div class="carousel-item">
                                 <img src="https://picsum.photos/id/28/1200/400" class="img-fluid p-3 rounded"
                                     alt="...">
                             </div>
                             <div class="carousel-item">
                                 <img src="https://picsum.photos/id/29/1200/400" class="img-fluid p-3 rounded"
                                     alt="...">
-                            </div>
+                                </div>
                         </div>
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"
                             data-bs-slide="prev">
@@ -42,38 +41,41 @@
                         </button>
                     </div>
 
-                    <div class="card shadow ms-3" style='width: 75rem;'>
-                        <div class="card-body">
+                   
 
-
-                            <h5 class="card-title">Titolo: {{ $announcement_to_check->title }}</h5>
-                            <p class="card-text">Descrizione: {{ $announcement_to_check->body }}</p>
-                            <p class="card-footer">Pubblicato il:
-                                {{ $announcement_to_check->created_at->format('d/m/y') }}</p>
+                        <div class="card shadow ms-3" style='width: 75rem;'>
+                            <div class="card-body">
+                                
+                                
+                                <h5 class="card-title">Titolo: {{ $announcement_to_check->title }}</h5>
+                                <p class="card-text">Descrizione: {{ $announcement_to_check->body }}</p>
+                                <p class="card-footer">Pubblicato il:
+                                    {{ $announcement_to_check->created_at->format('d/m/y') }}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+               
             <div class="row">
                 <div class="col-12 col-md-6">
                     <form
-                        action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
-                        method="POST">
-                        @csrf
-                        @method('PATCH')
-                        <button type="submit" class="btn btn-success shadow">Accetta</button>
-                    </form>
-                </div>
-                <div class="col-12 col-md-6 text-end">
-                    <form
-                        action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
-                        method="POST">
-                        @csrf
-                        @method('PATCH')
-                        <button type="submit" class="btn btn-danger shadow">Rifiuta</button>
-                    </form>
-                </div>
+                    action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
+                    method="POST">
+                    @csrf
+                    @method('PATCH')
+                    <button type="submit" class="btn btn-success shadow">Accetta</button>
+                </form>
             </div>
+            <div class="col-12 col-md-6 text-end">
+                <form
+                action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
+                method="POST">
+                @csrf
+                @method('PATCH')
+                <button type="submit" class="btn btn-danger shadow">Rifiuta</button>
+            </form>
         </div>
-    @endif
+    </div>
+</div>
+@endif
 </x-layout>
