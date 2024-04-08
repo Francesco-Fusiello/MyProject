e345067af204e91f1cb62ce8f1b687a4479d704b
Yvan <yvan@192.168.1.9>
2024-04-08 18:22:30 +0200

    test

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -36,6 +36,7 @@ class CreateNewUser implements CreatesNewUsers
             'email' => $input['email'],
             'is_revisor' => $this->is_revisor=false,
             'password' => Hash::make($input['password']),
+            'is_revisor'=> $input['is_revisor'],
         ]);
     }
 }
