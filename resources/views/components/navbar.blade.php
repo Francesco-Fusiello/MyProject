fea495c78a708bd7f9d7506673ce97de4a0f4c83
LuanaCanozzi <luana.canozzi@tiscali.it>
2024-04-11 19:53:29 +0200

    cambio nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,7 +12,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Homepage</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page"
