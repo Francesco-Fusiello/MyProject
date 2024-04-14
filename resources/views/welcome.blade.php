d2ea66ae5cfd53f19896041752e50036dc025500
LuanaCanozzi <luana.canozzi@tiscali.it>
2024-04-14 10:28:28 +0200

    finite categorie in home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,26 +23,49 @@
     <div class="container">
         <div class="row justify-content-center">
             <ul class="wrapper">
-                <li class="icon twitter">
+                <li class="icon abbigl">
                     <span class="tooltip">Abbigliamento</span>
                     <span><a class="iconaante" href="{{route('categoryShow', 1)}}"><i class="fa-solid fa-shirt"></i></a></span>
                 </li>
-                <li class="icon facebook">
+                <li class="icon inform">
                     <span class="tooltip">Informatica</span>
                     <span><a class="iconaante" href="{{route('categoryShow', 2)}}"><i class="fa-solid fa-computer"></a></i></span>
                 </li>
-                <li class="icon github">
+                <li class="icon elettro">
                     <span class="tooltip">Elettrodomestici</span>
                     <span><a class="iconaante" href="{{route('categoryShow', 3)}}"><i class="fa-solid fa-blender-phone"></a></i></span>
                 </li>
-                <li class="icon instagram">
+                <li class="icon libri">
                     <span class="tooltip">Libri</span>
                     <span><a class="iconaante" href="{{route('categoryShow', 4)}}"><i class="fa-solid fa-book"></a></i></span>
                 </li>
-                <li class="icon youtube">
-                    <span class="tooltip">Youtube</span>
-                    <span><i class="fab fa-youtube"></i></span>
+                <li class="icon giochi">
+                    <span class="tooltip">Giochi</span>
+                    <span><a class="iconaante" href="{{route('categoryShow', 5)}}"><i class="fa-solid fa-table-tennis-paddle-ball"></a></i></span>
                 </li>
+                <li class="icon sport">
+                    <span class="tooltip">Sport</span>
+                    <span><a class="iconaante" href="{{route('categoryShow', 6)}}"><i class="fa-solid fa-person-biking"></a></i></span>
+                </li>
+                <li class="icon telef">
+                    <span class="tooltip">Telefonia</span>
+                    <span><a class="iconaante" href="{{route('categoryShow', 7)}}"><i class="fa-solid fa-mobile-screen-button"></a></i></span>
+                </li>
+                <li class="icon servizi">
+                    <span class="tooltip">Servizi</span>
+                    <span><a class="iconaante" href="{{route('categoryShow', 8)}}"><i class="fa-regular fa-lightbulb"></a></i></span>
+                </li>
+                <li class="icon arredo">
+                    <span class="tooltip">Arredamento</span>
+                    <span><a class="iconaante" href="{{route('categoryShow', 9)}}"><i class="fa-solid fa-couch"></a></i></span>
+                </li>
+                <li class="icon vgame">
+                    <span class="tooltip">Videogame</span>
+                    <span><a class="iconaante" href="{{route('categoryShow', 10)}}"><i class="fa-solid fa-gamepad"></a></i></span>
+                </li>
+
+
+
             </ul>
         </div>
     </div>
