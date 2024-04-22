4868f54a92a524787c4d97144d7f911cccb77bd4
LuanaCanozzi <luana.canozzi@tiscali.it>
2024-04-22 20:48:35 +0200

    finita US 7

--- app/Livewire/CreateAnnouncement.php
+++ app/Livewire/CreateAnnouncement.php
@@ -8,6 +8,7 @@ use App\Jobs\ResizeImage;
 use App\Models\Announcement;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
+use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
@@ -94,6 +95,7 @@ class CreateAnnouncement extends Component
 
                 dispatch(new ResizeImage($newImage->path , 256 , 256));
                 dispatch(new GoogleVisionSafeSearch($newImage->id));
+                dispatch(new GoogleVisionLabelImage($newImage->id));
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
