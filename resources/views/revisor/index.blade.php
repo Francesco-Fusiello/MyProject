64d5213ec4d9bfb6d864304ad514821ebfad81b0
LuanaCanozzi <luana.canozzi@tiscali.it>
2024-04-23 17:29:55 +0200

    sistemato carousel revisore

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -8,7 +8,7 @@
 
     <div class='container-fluid bg-gradient shadow mb-3'>
         <div class="row d-flex justify-content-center ">
-            <div class="col-sd-12 col-lg-9 p-2">
+            <div class="col-12 p-2">
                 <h2 class="text-center">
                     @if ($announcement_to_check)
                         {{ __('ui.titRev') }}
@@ -23,79 +23,76 @@
 
     <div class="container">
         <div class="row d-flex justify-content-center">
-            <div class="col-sd-12 col-lg-9">
+            <div class="col-12 col-md-9 col-lg-9">
                 @if ($announcement_to_check)
                     <div id="carouselExampleFade" class="carousel slide carousel-fade">
                         @if ($announcement_to_check && $announcement_to_check->images && $announcement_to_check->images->count() > 0)
-                            <div class="container">
-                                <div class="carousel-inner row">
-
+                            <div class="container d-flex ">
+                                <div class="carousel-inner">
                                     @foreach ($announcement_to_check->images as $image)
-                                        <div class="carousel-item @if ($loop->first) active @endif">
-                                            <img src="{{ $image->getUrl(256, 256) }}" class=" col-md-6 img-fluid p-3 rounded"
-                                                alt="...">
-
-{{-- tag per google Vision --}}
-                                        <div class="col-md-3  d-flex justify-content-end">
-                                            <h5 class="tc-accent mt-3">Tags</h5>
-                                                <div class="p-2">
-                                                    @if($image->labels)
-                                                    @foreach ($image->labels as $label)
-                                                    <p class="d-inline">{{$label}},</p>
-                                                    @endforeach
+                                        <div class="carousel-item  d-flex @if ($loop->first)active @endif">
+                                            <div class="col-12 col-md-6 img-fluid p-3 rounded" alt="...">
+                                                <img src="{{ $image->getUrl(256, 256) }}" alt="immagine dell'articolo">
+                                            </div>
+                                            {{-- tag per google Vision --}}
+                                            <div class="col-12 col-md-3  d-flex justify-content-end">
+                                                <h5 class="tc-accent mt-3">Tags</h5>
+                                                <div class="p-2 fs-6">
+                                                    @if ($image->labels)
+                                                        @foreach ($image->labels as $label)
+                                                            <p class="d-inline">{{ $label }},</p>
+                                                        @endforeach
                                                     @endif
                                                 </div>
-                                        </div>
+                                            </div>
 
-                                        <div class="col-md-3">
-                                            <div class="card-body">
-                                                <h5 class="tc-accent">Revisione Immagini</h5>
-                                                <p>Adulti: <span class="{{$image->adult}}"></span> </p>
-                                                <p>Satira: <span class="{{$image->spoof}}"></span> </p>
-                                                <p>Medicina: <span class="{{$image->medical}}"></span> </p>
-                                                <p>Violenza: <span class="{{$image->violence}}"></span> </p>
-                                                <p>Conenuto Ammiccante: <span class="{{$image->racy}}"></span> </p>
+                                            <div class="col-12 col-md-3">
+                                                <div class="card-body">
+                                                    <h5 class="tc-accent">Revisione Immagini</h5>
+                                                    <p>Adulti: <span class="{{ $image->adult }}"></span> </p>
+                                                    <p>Satira: <span class="{{ $image->spoof }}"></span> </p>
+                                                    <p>Medicina: <span class="{{ $image->medical }}"></span> </p>
+                                                    <p>Violenza: <span class="{{ $image->violence }}"></span> </p>
+                                                    <p>Conenuto Ammiccante: <span class="{{ $image->racy }}"></span>
+                                                    </p>
+                                                </div>
                                             </div>
                                         </div>
-
-                                    </div>
-                                    </div>
                                     @endforeach
-
-                            @else
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <img src="https://picsum.photos/id/28/1200/400" class="img-fluid p-3 rounded"
-                                            alt="...">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src="https://picsum.photos/id/29/1200/400" class="img-fluid p-3 rounded"
-                                            alt="...">
-                                    </div>
-                                    <div class="carousel-item">
-                                        <img src="https://picsum.photos/id/29/1200/400" class="img-fluid p-3 rounded"
-                                            alt="...">
-                                    </div>
+                                </div>
+                            </div>
+                        @else
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="https://picsum.photos/id/28/1200/400" class="img-fluid p-3 rounded"
+                                        alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://picsum.photos/id/29/1200/400" class="img-fluid p-3 rounded"
+                                        alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://picsum.photos/id/29/1200/400" class="img-fluid p-3 rounded"
+                                        alt="...">
                                 </div>
                             </div>
                         @endif
+
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"
+                                    data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"
+                                    data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-
-
+                        {{-- </div>
+                    </div> --}}
                     <div class="card shadow ms-3" style='width: 58rem;'>
                         <div class="card-body">
-
-
                             <h5 class="card-title">{{ __('ui.titolo') }}: {{ $announcement_to_check->title }}</h5>
                             <p class="card-text">{{ __('ui.descri') }}: {{ $announcement_to_check->body }}</p>
                             <p class="card-footer">{{ __('ui.pub') }}
@@ -103,12 +100,9 @@
                         </div>
                     </div>
 
-
-
                     <div class="row d-flex justify-content-between ">
                         <div class="col-6 text-center ">
-                            <form
-                                action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
+                            <form action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
                                 method="POST">
                                 @csrf
                                 @method('PATCH')
@@ -116,8 +110,7 @@
                             </form>
                         </div>
                         <div class="col-6 text-center">
-                            <form
-                                action="{{ route('revisor.reject_announcement', ['announcement' => $announcement_to_check]) }}"
+                            <form action="{{ route('revisor.reject_announcement', ['announcement' => $announcement_to_check]) }}"
                                 method="POST">
                                 @csrf
                                 @method('PATCH')
@@ -127,16 +120,17 @@
                     </div>
 
                 @endif
-                <div class="row">
-                    <div class="col-12 text-center justify-content-center">
-                        <form method="GET" action="{{ route('announcements.reset-last-accepted') }}">
-                            @csrf
-                            <button type="submit" class="bn632-hover yellow">{{ __('ui.annu') }}</button>
-                        </form>
+
+                    <div class="row">
+                        <div class="col-12 text-center justify-content-center">
+                            <form method="GET" action="{{ route('announcements.reset-last-accepted') }}">
+                                @csrf
+                                <button type="submit" class="bn632-hover yellow">{{ __('ui.annu') }}</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
             </div>
         </div>
     </div>
-   
+
 </x-layout>
