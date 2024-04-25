71bbdaca7c27f2dd938ce61e25e230eb1cc5179e
Franesco Fusiello <ffusiello22@gmail.com>
2024-04-25 16:08:29 +0200

    sistemato alert e required front-end

--- resources/views/components/barraricerca.blade.php
+++ resources/views/components/barraricerca.blade.php
@@ -5,7 +5,7 @@
             <form action="{{ route('announcement.search') }}" method="GET" class="d-flex">
                 <input type="search" name="searched" class="form-control me-2" style="width: 100%"
                     placeholder='{{__('ui.searchart')}}' aria-label='Search'>
-                <button class="btn btn-outline-primary w-25 " type='submit'>{{__('ui.search')}}</button>
+                <button class="btn btn-outline-primary w-25" type='submit'>{{__('ui.search')}}</button>
             </form>
         </div>
     </div>
