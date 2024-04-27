f31c2aae1a06d19ffab3d65b907974529c6d383f
Franesco Fusiello <ffusiello22@gmail.com>
2024-04-27 17:38:35 +0200

    creato Chi-siamo responsive

--- resources/views/chiSiamo.blade.php
+++ resources/views/chiSiamo.blade.php
@@ -10,7 +10,7 @@
         </div>
     </header>
 
-    <div class="container justify-content-center fst-italic ">
+    <div class="container justify-content-center fst-italic col-9 ">
         <h5 class=" d-flex justify-content-center fst-italic">FLAY è un acronimo, dei fondatori Francesco, Luana, Antony, ed Yvan..</h5> 
         <p class="d-flex justify-content-center fst-italic"> Siamo un gruppo di imprenditori molto giovani ma con alle spalle l'esperienza della nostra (Luana),  partiti con l'idea di creare una
             piattaforma di vendita di prodotti nuovi, usati, e di servizi intellettuali, il quele ormai diventata REALTA'... <br>
@@ -23,7 +23,7 @@
     
 
     <div class="containerCard">
-        <div class="box">
+        <div class="box col-12">
             <div class="imgBoxCard">
                 <img src="https://img.freepik.com/free-photo/portrait-handsome-young-man-makes-okay-gesture-demonstrates-agreement-likes-idea-smiles-happily-wears-optical-glasses-yellow-hat-t-shirt-models-indoor-its-fine-thank-you-hand-sign_273609-30676.jpg?size=626&ext=jpg" alt="">
             </div>
@@ -32,7 +32,7 @@
                 <span>Full Stack Web Developer</span></h2>
             </div>
         </div>
-        <div class="box">
+        <div class="box col-12">
             <div class="imgBoxCard">
                 <img src="https://image.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg" alt="">
             </div>
@@ -41,7 +41,7 @@
                 <span>Full Stack Web Developer</span></h2>
             </div>
         </div>
-        <div class="box">
+        <div class="box col-12">
             <div class="imgBoxCard">
                 <img src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg" alt="">
             </div>
@@ -50,7 +50,7 @@
                 <span>Full Stack Web Developer</span></h2>
             </div>            
         </div>
-        <div class="box">
+        <div class="box col-12">
             <div class="imgBoxCard">
                 <img src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg" alt="">
             </div>
