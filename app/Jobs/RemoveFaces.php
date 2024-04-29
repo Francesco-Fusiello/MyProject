706b335ba3b5bb33924f0464e4d016e2e9c2d628
Yvanesse <y.sacco96@gmail.com>
2024-04-29 20:03:58 +0200

    modificati /

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -58,7 +58,7 @@ class RemoveFaces implements ShouldQueue
 
             $image = SpatieImage::load($srcPath);
 
-            $image->watermark(base_path('resources\img\blurtondo.png'))
+            $image->watermark(base_path('resources/img/blurtondo.png'))
             ->watermarkPosition('top-left')
             ->watermarkPadding($bounds[0][0], $bounds[0][1])
             ->watermarkWidth($w, Manipulations::UNIT_PIXELS)
